e16e9f0e8f734fee0b6a3a245de4ff9bab278b98
Trevor Edwards <trevore23@gmail.com>
2019-05-13 16:43:03 +1000

    Updated admin to support custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,4 +26,14 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test1')
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@trevor.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
